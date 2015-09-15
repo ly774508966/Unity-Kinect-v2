21fbe35c11a65dfda89c11705d0afd8941d39bb0
Adityo Setyonugroho <adityosn@hotmail.com>
2015-09-16 01:49:25 +0700

    Unity with Kinect v2
    Basic sample of unity project with Kinect v2

---
+++ Assets/KinectScripts/JointPoint.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using Windows.Kinect;
+
+public class JointPoint : MonoBehaviour {
+
+    public GameObject BodySourceManager;
+    public JointType TrackedJoint;
+    private BodySourceManager bodyManager;
+    private Body[] bodies;
+    public GameObject attachObject;
+
+    // Use this for initialization
+    void Start () {
+	    if(BodySourceManager == null)
+        {
+            Debug.Log("Attach Body Source Manager");
+        }
+        else
+        {
+            bodyManager = BodySourceManager.GetComponent<BodySourceManager>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(bodyManager == null)
+        {
+            return;
+        }
+        bodies = bodyManager.GetData();
+
+        if(bodies == null)
+        {
+            return;
+        }
+        foreach(var body in bodies)
+        {
+            if(body == null)
+            {
+                continue;
+            }
+            if (body.IsTracked)
+            {
+                //Object Position
+                var pos = body.Joints[TrackedJoint].Position;
+                attachObject.transform.position = new Vector3(pos.X * 10, pos.Y * 10, pos.Z * 10);
+
+                //Object Orientation
+                var orientation = body.JointOrientations[TrackedJoint].Orientation;
+                attachObject.transform.rotation = new Quaternion(orientation.X, orientation.Y, orientation.Z, 
+                    orientation.W);
+
+                /*//Object Orientation
+                var orientation = body.JointOrientations[TrackedJoint].Orientation;
+                attachObject.transform.localRotation = new Quaternion(orientation.X, orientation.Y, orientation.Z,
+                    orientation.W);*/
+
+                /*//Object Orientation
+                var orientation = body.JointOrientations[TrackedJoint].Orientation;
+                attachObject.transform.localRotation = Quaternion.Euler( -90f, 0f, 0f ) * new Quaternion(orientation.X, 
+                orientation.Y, orientation.Z, orientation.W);*/
+            }
+        }
+
+	}
+}
---
+++ Assets/PointerPos.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using Windows.Kinect;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PointerPos : MonoBehaviour {
+
+    private KinectSensor kinectSensor;
+    private ColorSpacePoint colorSpacePoint;
+
+    public GameObject BodySourceManager;
+    private BodySourceManager _BodyManager;
+
+    void Start()
+    {
+        kinectSensor = KinectSensor.GetDefault();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (BodySourceManager == null)
+        {
+            return;
+        }
+
+        _BodyManager = BodySourceManager.GetComponent<BodySourceManager>();
+        if (_BodyManager == null)
+        {
+            return;
+        }
+
+        Windows.Kinect.Body[] data = _BodyManager.GetData();
+        if (data == null)
+        {
+            return;
+        }
+
+        foreach (var body in data)
+        {
+            if (body == null)
+            {
+                continue;
+            }
+
+            if (body.IsTracked)
+            {
+                CameraSpacePoint cameraSpacePoint = body.Joints[Windows.Kinect.JointType.HandLeft].Position;
+                colorSpacePoint = kinectSensor.CoordinateMapper.MapCameraPointToColorSpace(cameraSpacePoint);
+            }
+        }
+    }
+}
